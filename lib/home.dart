660e5aa76c46c09279298ba3e1159fcebd466e8e
dna2121 <dnatayawibisono@gmail.com>
2022-01-03 17:19:33 +0700

    login google button

--- lib/home.dart
+++ lib/home.dart
@@ -7,112 +7,132 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          // color: Colors.red,
-          margin: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        body: Center(
+          child: Container(
+            //color: Colors.red,
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 60),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Hello,",
-                          style: TextStyle(fontSize: 30),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 60),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Hello,",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Text(
+                              "Welcome",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ],
                         ),
-                        Text(
-                          "Welcome",
-                          style: TextStyle(fontSize: 30),
+                      ),
+                      Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Email",
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.deepPurple)),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Password",
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.deepPurple)),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 33,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 55,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.deepPurple),
+                              ),
+                              child: const Text(
+                                "Sign In",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 87),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "— Or —",
+                          style: TextStyle(color: Colors.black26, fontSize: 20),
+                        ),
+                        const SizedBox(height: 23),
+                        Container(
+                          width: double.infinity,
+                          height: 55,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                    side: const BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image.network(
+                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png',
+                                  height: 20,
+                                ),
+                                const SizedBox(width: 10),
+                                const Text(
+                                  "Sign In with Google",
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.deepPurple),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Email",
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.deepPurple)),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Password",
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.deepPurple)),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 33,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: 55,
-                        child: TextButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                            ),
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.deepPurple),
-                          ),
-                          child: const Text(
-                            "Sign In",
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.only(top: 87),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "— Or —",
-                      style: TextStyle(color: Colors.black26, fontSize: 20),
-                    ),
-                    const SizedBox(height: 23),
-                    Container(
-                      width: double.infinity,
-                      height: 55,
-                      child: TextButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                                side: BorderSide(color: Colors.grey)),
-                          ),
-                        ),
-                        child: const Text(
-                          "Sign In with Google",
-                          style:
-                              TextStyle(fontSize: 18, color: Colors.deepPurple),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
