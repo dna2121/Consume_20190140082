0b9ccede5b933631dee6599eb34ad9b8743d162d
dna2121 <dnatayawibisono@gmail.com>
2022-01-03 15:18:38 +0700

    column sign in form

--- lib/home.dart
+++ lib/home.dart
@@ -31,46 +31,53 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "Email",
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple)),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "Password",
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple)),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 33,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 55,
-                    child: TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
+                  Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.deepPurple)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Password",
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.deepPurple)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 33,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 55,
+                        child: TextButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.deepPurple),
+                          ),
+                          child: const Text(
+                            "Sign In",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18),
                           ),
                         ),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.deepPurple),
                       ),
-                      child: const Text(
-                        "Sign In",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ),
+                    ],
                   ),
                 ],
               ),
